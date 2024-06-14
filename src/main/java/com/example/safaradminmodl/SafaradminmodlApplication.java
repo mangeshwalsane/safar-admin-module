b3ad5ae78c3dbf36ec2575c6b02b748e63311371
Mangesh Walsane <mangeshwalsane@Mangeshs-MacBook-Air.local>
2024-06-14 16:31:22 +0530

    Initial commit with Spring Boot project setup

---
+++ src/main/java/com/example/safaradminmodl/SafaradminmodlApplication.java
@@ -0,0 +1,13 @@
+package com.example.safaradminmodl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafaradminmodlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafaradminmodlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/safaradminmodl/SafaradminmodlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safaradminmodl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafaradminmodlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
